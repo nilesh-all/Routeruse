3cdcf2bffcfff7d7229c99491471569b379ba4f5
nilesh-all <nileshmagarde0@gmail.com>
2024-12-09 11:27:37 +0530

    react router

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './component/Header/Header'
+import Footer from './component/Footer/Footer'
+import Home from './component/Home/Home'
+
+function App() {
+  
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/component/User/User.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+function User (){
+    const {userid} = useParams()
+
+    return<>
+    <div className="flex justify-center bg-green-200">User: {userid}</div>
+    </>
+}
+
+export default User
